675e3dd466a3aa63da1648e24a0143961ff65203
EdoriAtiri <edoriatiri@gmail.com>
2021-08-23 15:40:57 +0100

    switched to async await to fetch data and added some style changes

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const results = document.getElementById('results');
 const result_heading = document.getElementById('result_heading');
 const form = document.getElementById('form');
 
-const client_id = "=";
+const client_id = "";
 
 
-function search_image(e) {
+/* function search_image(e) {
     e.preventDefault();
 
     // get search term
@@ -42,9 +42,46 @@ function search_image(e) {
         });
 
 
+} */
+
+async function search_image (e) {
+    e.preventDefault()
+
+    // get search term
+    const term = search.value.trim();
+    const url = `https://api.unsplash.com/search/photos/?client_id=${client_id}&query=${term}`;
+    
+    try {
+        let res = await fetch(url)
+        let data = await res.json()
+        console.log(data)
+
+         result_heading.innerHTML = `<h2>Search Results for '${term}'</h2>`;
+
+            if (data.results.length === 0) {
+                results.innerHTML = `<p style="font-size:5em;text-align:center;">😔</p>`;
+                result_heading.innerHTML = `<p>There are no search results for '${term}', please try again</p>`;
+            } else {
+                results.innerHTML = data.results.map(data =>
+                    `<div><img src="${data.urls.regular}" alt="${data.alt_description}">
+                    <p class="author">
+                     Photo by <a href="${data.user.links.html}?utm_source=ImG_Search&utm_medium=referral">${data.user.username}</a> on <a
+        href="https://unsplash.com/?utm_source=ImG_Search&utm_medium=referral">Unsplash</a>
+</p>
+<a class="download" href="${data.links.download}" download><i class="fa fa-arrow-down" aria-hidden="true"></i>
+    </a>
+</div>`
+                ).join(' ');
+
+            }
+    } catch (error) {
+        console.error(error)
+    }
+
+    
+
+   
 }
 
-
-const new_img = `<div><img src="images/image6.jpeg" alt=""></div>`;
 
 form.addEventListener('submit', search_image);
