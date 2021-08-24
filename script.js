761da7a6da0b6ce84c1a878a07adf599fdd3132a
EdoriAtiri <edoriatiri@gmail.com>
2021-08-24 19:09:41 +0100

    added loading animation and some css changes

--- script.js
+++ script.js
@@ -3,21 +3,45 @@ const submit = document.getElementById('submit');
 const results = document.getElementById('results');
 const result_heading = document.getElementById('result_heading');
 const form = document.getElementById('form');
+const roller = document.querySelector(".lds-roller")
+const input = document.querySelector("input[type='text']")
+const err = document.querySelector(".err")
+
 
 async function search_image (e) {
     e.preventDefault()
-
+  
+    
     // get search term
     const term = search.value.trim();
- const url = `/.netlify/functions/fetch-image?term=${term}`;
-
+    const url = `/.netlify/functions/fetch-image?term=${term}`;
+    
+     if (term === "") {
+                   roller.style.display = "none"
+                  
+                           err.style.opacity = '1'
+                           input.style.boxShadow = '0 0 0 2px #DF2E2E'
+                    
+                   setTimeout(() => {
+                       err.style.opacity = '0'
+                           input.style.boxShadow = '0 0 0 2px #d7e1ec'
+                   }, 3000);
+                //  result_heading.innerHTML = `<h3>Please enter a search term</h3>`
+            } else {
+                  roller.style.display = "inline-block"
+    results.innerHTML = ""
+    results.appendChild(roller)
+    result_heading.innerHTML = `<p>searching for '${term}'</p>`
+            
     try {
         let res = await fetch(url)
         let data = await res.json()
 
          result_heading.innerHTML = `<h2>Search Results for '${term}'</h2>`;
 
-            if (data.results.length === 0) {
+           if (data.results.length === 0) {
+                                   roller.style.display = "none"
+
                 results.innerHTML = `<p style="font-size:5em;text-align:center;">😔</p>`;
                 result_heading.innerHTML = `<p>There are no search results for '${term}', please try again</p>`;
             } else {
@@ -32,10 +56,15 @@ async function search_image (e) {
 </div>`
                 ).join(' ');
 
+                setTimeout(() => {
+                    search.value = ""
+                }, 5000);
+
             }
     } catch (error) {
         console.error(error)
     }
+}
 }
 
 form.addEventListener('submit', search_image);
